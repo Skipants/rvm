762ccb2324f5ba55618c7279c22bef16a2e5890c
Andrew Szczepanski <aszczepanski@financeit.io>
2022-02-22 16:34:54 -0500

    Try fixing resources

--- resources/system_install.rb
+++ resources/system_install.rb
@@ -1,4 +1,7 @@
 unified_mode true
+
+resource_name :rvm_system_install
+
 property :key_server, String, default: 'hkp://pool.sks-keyservers.net'
 property :gpg_homedir, String, default: '~/.gnupg'
 property :gpg_key, String, default: '409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB'
